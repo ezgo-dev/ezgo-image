586d2280526d64b02bb57276348ee5b3933e0a77
AceLan Kao <acelan@acelan.idv.tw>
2018-03-23 20:53:08 +0800

    Update rootfs to 18.04 and remove old packages
    1. Update the docker rootfs to Ubuntu 18.04
    2. remove some old packages that do not exist in 18.04
    3. using wine in official archive
    
    Signed-off-by: AceLan Kao <acelan@acelan.idv.tw>

--- build.sh
+++ build.sh
@@ -34,20 +34,20 @@ installpackages+="gstreamer1.0-libav "
 installpackages+="gstreamer1.0-vaapi "
 
 # for ezgo
-installpackages+="vim vim-gtk gksu kdesudo zenity im-switch chromium-browser language-pack-zh-hant firefox-locale-zh-hant language-pack-zh-hant-base language-pack-gnome-zh-hant language-pack-kde-zh-hant kde-l10n-zhtw kdeedu kdegraphics kdemultimedia firefox "
+installpackages+="vim vim-gtk gksu kdesudo zenity im-switch chromium-browser language-pack-zh-hant firefox-locale-zh-hant language-pack-zh-hant-base language-pack-gnome-zh-hant language-pack-kde-zh-hant kde-l10n-zhtw firefox "
 # hime
 installpackages+="hime hime-qt5-immodule hime-gtk3-immodule hime-chewing hime-anthy "
 # sigil, kazam, blender, and ffdiaporama
 installpackages+="sigil kazam blender ffdiaporama "
 # kdegames
-installpackages+="kdegames supertux supertuxkart pingus tuxmath tuxtype "
+installpackages+="supertux supertuxkart pingus tuxmath tuxtype "
 # scribus
 installpackages+="scribus "
 installpackages+="tuxpaint mypaint inkscape gimp digikam gthumb "
 installpackages+="openshot kdenlive vlc totem cheese guvcview brasero amarok audacious audacity tuxguitar musescore "
 installpackages+="qtqr furiusisomount hydrogen winff qjackctl ardour filezilla pidgin remmina pcmanx-gtk2 stellarium kayali bkchem ghemical avogadro gchempaint pymol scratch "
 # wine
-installpackages+="wine1.6 winetricks wine-gecko wine-mono "
+installpackages+="wine-stable winetricks "
 
 # Packages that will be removed:
 removepackages="kmail kontact rekonq korganizer ubuntu-release-upgrader-qt update-manager-core muon-notifier "
@@ -58,13 +58,10 @@ sync;sync;sync
 ./isorespin.sh -i $ISOFILE \
 	$KERNELARGS \
 	-r "ppa:graphics-drivers/ppa" \
-	-r "ppa:ubuntu-wine/ppa" \
 	-p "$installpackages" \
 	-f wrapper-network.sh \
 	-f wrapper-nvidia.sh \
 	-f ezgo \
 	-c wrapper-network.sh \
 	-c wrapper-nvidia.sh \
-	-c ezgo/ezgo.sh \
-	-g "" \
-	-g "quiet splash acpi_rev_override=1"
+	-c ezgo/ezgo.sh
