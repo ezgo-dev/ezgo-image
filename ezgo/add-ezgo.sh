5d9a4921dcf7677ceb51b057c9a70a81b035a7ee
AceLan Kao <acelan@acelan.idv.tw>
2018-05-16 08:38:12 +0800

    add clean after installed ezgo packages
    Signed-off-by: AceLan Kao <acelan@acelan.idv.tw>

--- ezgo/add-ezgo.sh
+++ ezgo/add-ezgo.sh
@@ -12,6 +12,8 @@ apt update 2>&1
 apt install -y ezgo-artwork ezgo-menu ezgo-kde5 ezgo-phet ezgo-usgs ezgo-npa ezgo-chem ezgo-gsyan ezgo-wordtest ezgo-misc-arduino-rules ezgo-misc-decompress ezgo-misc-desktop-files ezgo-misc-furiusisomount ezgo-misc-inkscape ezgo-misc-installer ezgo-misc-kdenlive ezgo-misc-klavaro ezgo-misc-ktuberling ezgo-misc-qtqr ezgo-misc-winff 2>&1
 
 apt purge -y akonadi-backend-mysql mysql-server kmail konversation ktnef kontact rekonq korganizer ubuntu-release-upgrader-qt update-manager-core muon-notifier
+apt clean
+apt autoclean
 
 # ezgo wallpaper theme
 sed -i 's/defaultWallpaperTheme=.*/defaultWallpaperTheme=ezgo/' /usr/share/plasma/desktoptheme/*/metadata.desktop
